71b1cf194dd1ada589b4b8341f148c38165becaf
andrea.lovrinovic <andrea.lovrinovic@united.group>
2024-09-17 10:57:28 +0200

    create new build

--- dist/serto-bel-hr/server/main.server.mjs
+++ dist/serto-bel-hr/server/main.server.mjs
@@ -1,6 +1,2 @@
-import "./polyfills.server.mjs";
-import { a } from "./chunk-MQ4VD6AU.mjs";
-import "./chunk-7SIFG734.mjs";
-import "./chunk-PW67DGCK.mjs";
-import "./chunk-5XUXGTUW.mjs";
-export { a as default };
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZAGG4T2Y.mjs";import"./chunk-7SIFG734.mjs";import"./chunk-PW67DGCK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
